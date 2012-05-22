f834e0c0c3eac9922bb1f8a6ca67ece8c9e9fd83
Guy John <guy@rumblesan.com>
2012-05-22 22:11:48 +0100

    Cleaning up imports

--- install.py
+++ install.py
@@ -1,44 +1,41 @@
 #!/usr/bin/env python
 
 from glob import glob
-from os.path import isdir, lexists, islink
-from os import getenv, unlink, rmdir, symlink, getcwd
 import os
-import os.path
-from  sys import argv
+from sys import argv
 
 
 def main(argv):
     if len(argv) < 2:
         argv.append("install")
-    home = getenv("HOME")
+    home = os.getenv("HOME")
     files = glob('*.symlink')
     for filename in files:
         link = ".%s" % filename.split(".")[0]
         linkPath = os.path.join(home, link)
-        if lexists(linkPath):
+        if os.path.lexists(linkPath):
             removeFile(link)
         if argv[1] == "install":
             linkFile(filename, link)
 
 def removeFile(link):
-    home = getenv("HOME")
+    home = os.getenv("HOME")
     target = os.path.join(home, link)
     print("removing %s" % target)
-    if islink(target):
-        unlink(target)
-    elif isdir(target):
-        rmdir(target)
+    if os.path.islink(target):
+        os.unlink(target)
+    elif os.path.isdir(target):
+        os.rmdir(target)
     else:
-        unlink(target)
+        os.unlink(target)
 
 def linkFile(filename, target):
-    dotfilesDir  = getcwd()
+    dotfilesDir  = os.getcwd()
     filename = os.path.join(dotfilesDir, filename)
-    home = getenv("HOME")
+    home = os.getenv("HOME")
     os.chdir(home)
     print("symlinking %s -> %s" % (filename, target))
-    symlink(filename, target)
+    os.symlink(filename, target)
     os.chdir(dotfilesDir)
 
 if __name__ == "__main__":
