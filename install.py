5b658877015fd026d59df570d4e4cc4bc944a72d
Guy John <guy@rumblesan.com>
2012-05-24 17:49:51 +0100

    Adding in basic templating for files

--- install.py
+++ install.py
@@ -2,10 +2,12 @@
 
 from glob import glob
 import os
-from sys import argv
 
 import re
+from ConfigParser import SafeConfigParser
 from argparse import ArgumentParser
+
+from sys import exit
 
 regexp = re.compile(r"@%\{(.+?)}%@")
 
@@ -14,27 +16,87 @@ def parseArgs():
     parser.add_argument('command', metavar='command', default='install',
                         choices=['install', 'uninstall'],
                         help='command to run')
+    parser.add_argument('-k', action='store', nargs=1, dest='keys',
+                        metavar='keys', help='keys file to use for templating')
 
-    return parser.parse_args()
+    args =  parser.parse_args()
+    keys = {}
 
-def main(argv):
+    if args.keys:
+        keyParser = SafeConfigParser()
+        keyParser.read(args.keys)
+        keyList = keyParser.items('keys')
+        for k, v in keyList:
+            keys[k] = v
 
-    args = parseArgs()
+    return (args, keys)
 
-    home = os.getenv("HOME")
-    files = glob('*.symlink')
+def main():
+
+    args, keys = parseArgs()
+
+    dotfileDir = os.path.join(os.getenv("HOME"), '.dotfiles')
+    compiledDir = os.path.join(dotfileDir, 'compiled')
+    os.chdir(dotfileDir)
+
+    if args.command == "install":
+        cleanup(compiledDir)
+        findTemplates(compiledDir, keys)
+
+    findSymlinks(args, dotfileDir)
+    findSymlinks(args, compiledDir)
+
+    if args.command == "uninstall":
+        cleanup(compiledDir)
+
+def cleanup(directory):
+    files = glob('%s/*' % directory)
     for filename in files:
-        link = ".%s" % filename.split(".")[0]
-        linkPath = os.path.join(home, link)
+        os.unlink(filename)
+
+
+def findTemplates(outputDir, keys):
+    files = glob('*.tmplt')
+    for filename in files:
+        template(filename, outputDir, keys)
+
+def template(filename, outputDir, keys):
+
+    outName = os.path.splitext(os.path.split(filename)[-1])[0]
+    outFile = os.path.join(outputDir, outName)
+
+    rf = open(filename)
+    of = open(outFile, 'w')
+
+    for line in rf:
+        matches = regexp.search(line)
+        if matches:
+            matches = list(matches.groups())
+            for match in matches:
+                if match in keys:
+                    replacement = keys[match]
+                else:
+                    replacement = raw_input('Value for key %s:  ' % match)
+                of.write(regexp.sub(replacement, line))
+        else:
+            of.write(line)
+
+    rf.close()
+    of.close()
+
+def findSymlinks(args, folder):
+    files = glob('%s/*.symlink' % folder)
+    for filename in files:
+        link = ".%s" % os.path.splitext(os.path.split(filename)[-1])[0]
+        print(filename, link)
+        linkPath = os.path.join(os.getenv("HOME"), link)
         if os.path.lexists(linkPath):
             removeFile(link)
         if args.command == "install":
             linkFile(filename, link)
 
-
 def removeFile(link):
-    home = os.getenv("HOME")
-    target = os.path.join(home, link)
+    target = os.path.join(os.getenv("HOME"), link)
     print("removing %s" % target)
     if os.path.islink(target):
         os.unlink(target)
@@ -46,12 +108,11 @@ def removeFile(link):
 def linkFile(filename, target):
     dotfilesDir  = os.getcwd()
     filename = os.path.join(dotfilesDir, filename)
-    home = os.getenv("HOME")
-    os.chdir(home)
+    os.chdir(os.getenv("HOME"))
     print("symlinking %s -> %s" % (filename, target))
     os.symlink(filename, target)
     os.chdir(dotfilesDir)
 
 if __name__ == "__main__":
-    main(argv)
+    main()
 
