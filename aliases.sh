219ff62b458ad991a3235351ba331b7132870922
Guy John <guy@rumblesan.com>
2012-07-07 13:05:46 +0100

    Adding some aliases
    Thanks to the dotfile examples of Mathias Bynens
    https://github.com/mathiasbynens

---
+++ aliases.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Easier navigation: .., ..., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Local IP addresses
+alias lip="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# Turn on colors for grep
+alias grep="grep --color"
+
+# By default, use normal ls and --color switch
+alias ls="ls -h --color"
+alias la="ls -ha --color"
+alias ll="ls -hl --color"
+alias lal="ls -hal --color"
+
+alias dotfiles="cd ${DOTFILES}"
