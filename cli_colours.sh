5f40cb1a0f74d626afd86dcbe7ed47790335fdcc
Guy John <guy@rumblesan.com>
2012-07-19 10:40:25 +0100

    Adding predefined CLI colours file
    Contains predefined colour variables using tputs

---
+++ cli_colours.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+export BLACK=$(tput setaf 0)
+export RED=$(tput setaf 1)
+export GREEN=$(tput setaf 2)
+export YELLOW=$(tput setaf 3)
+export LIME_YELLOW=$(tput setaf 190)
+export POWDER_BLUE=$(tput setaf 153)
+export BLUE=$(tput setaf 4)
+export MAGENTA=$(tput setaf 5)
+export CYAN=$(tput setaf 6)
+export WHITE=$(tput setaf 7)
+export BRIGHT=$(tput bold)
+export NORMAL=$(tput sgr0)
+export BLINK=$(tput blink)
+export REVERSE=$(tput smso)
+export UNDERLINE=$(tput smul)
+
