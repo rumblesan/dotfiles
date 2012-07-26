da131c9be95943d831aed70924ba3a7acd0ea3c6
Guy John <guy@rumblesan.com>
2012-07-26 05:13:58 -0400

    colours only defined if a terminal is present
    otherwise tputs throws an error. for example using ssh to run commands
    remotely

--- cli_colours.sh
+++ cli_colours.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
-export BLACK=$(tput setaf 0)
-export RED=$(tput setaf 1)
-export GREEN=$(tput setaf 2)
-export YELLOW=$(tput setaf 3)
-export LIME_YELLOW=$(tput setaf 190)
-export POWDER_BLUE=$(tput setaf 153)
-export BLUE=$(tput setaf 4)
-export MAGENTA=$(tput setaf 5)
-export CYAN=$(tput setaf 6)
-export WHITE=$(tput setaf 7)
-export BRIGHT=$(tput bold)
-export NORMAL=$(tput sgr0)
-export BLINK=$(tput blink)
-export REVERSE=$(tput smso)
-export UNDERLINE=$(tput smul)
+# If there's a tty then we can use tputs
+# If there's not then we don't define them
+# TODO I probablly should define them using escape codes
+if [[ $- =~ i ]]; then
+    export BLACK=$(tput setaf 0)
+    export RED=$(tput setaf 1)
+    export GREEN=$(tput setaf 2)
+    export YELLOW=$(tput setaf 3)
+    export LIME_YELLOW=$(tput setaf 190)
+    export POWDER_BLUE=$(tput setaf 153)
+    export BLUE=$(tput setaf 4)
+    export MAGENTA=$(tput setaf 5)
+    export CYAN=$(tput setaf 6)
+    export WHITE=$(tput setaf 7)
+    export BRIGHT=$(tput bold)
+    export NORMAL=$(tput sgr0)
+    export BLINK=$(tput blink)
+    export REVERSE=$(tput smso)
+    export UNDERLINE=$(tput smul)
+fi
 
