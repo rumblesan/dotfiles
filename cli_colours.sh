11cef9ab72551a06f2a1ed14cf115af9c0d0405b
Guy John <guy@rumblesan.com>
2012-07-30 11:15:55 +0100

    Fixing cli_colours to remove tputs usage
    Turns out it was causing issues with line wrapping in bash

--- cli_colours.sh
+++ cli_colours.sh
@@ -1,23 +1,15 @@
 #! /bin/bash
 
-# If there's a tty then we can use tputs
-# If there's not then we don't define them
-# TODO I probablly should define them using escape codes
-if [[ $- =~ i ]]; then
-    export BLACK=$(tput setaf 0)
-    export RED=$(tput setaf 1)
-    export GREEN=$(tput setaf 2)
-    export YELLOW=$(tput setaf 3)
-    export LIME_YELLOW=$(tput setaf 190)
-    export POWDER_BLUE=$(tput setaf 153)
-    export BLUE=$(tput setaf 4)
-    export MAGENTA=$(tput setaf 5)
-    export CYAN=$(tput setaf 6)
-    export WHITE=$(tput setaf 7)
-    export BRIGHT=$(tput bold)
-    export NORMAL=$(tput sgr0)
-    export BLINK=$(tput blink)
-    export REVERSE=$(tput smso)
-    export UNDERLINE=$(tput smul)
-fi
+BLACK="\[\033[30m\]"
+RED="\[\033[31m\]"
+GREEN="\[\033[32m\]"
+YELLOW="\[\033[33m\]"
+BLUE="\[\033[34m\]"
+MAGENTA="\[\033[35m\]"
+CYAN="\[\033[36m\]"
+WHITE="\[\033[37m\]"
+NORMAL="\[\033[0m\]"
+BOLD="\[\033[1m\]"
+REVERSE="\[\033[7m\]"
+UNDERLINE="\[\033[4m\]"
 
