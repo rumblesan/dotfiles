d420ea5b1b0b123d4ac4aa74154db89fadc0087d
Guy John <guy@rumblesan.com>
2013-04-30 18:00:42 +0100

    Adding completion script for repos function

---
+++ completions/repos_completions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+_complete_repos ()
+{
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        comp_repo_folders=`/bin/ls ~/repositories`
+        COMPREPLY=( $(compgen -W "${comp_repo_folders}" -- $cur))
+        return 0
+}
+complete -F _complete_repos repos
