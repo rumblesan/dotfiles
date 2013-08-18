cb93c77d029bf6da60e9f6cf29338565cabec5a1
Guy John <guy@rumblesan.com>
2013-08-18 21:45:21 +0100

    Adding helper function for hsenv

--- languages/haskell.sh
+++ languages/haskell.sh
@@ -6,3 +6,14 @@
 # Add cabal bin dir to path
 PATH=$PATH:~/Library/Haskell/bin
 
+# Simpler way to use hsenv
+haskell_env()
+{
+    if [ -f ./.hsenv/bin/activate ]; then
+        echo "Activating HSEnv"
+        source ./.hsenv/bin/activate
+    else
+        echo "No HSEnv present"
+    fi
+}
+
