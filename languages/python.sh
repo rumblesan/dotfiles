e444157867cd823a62d2fada61e0ad9db6c8f6bc
Guy John <guy@rumblesan.com>
2013-08-18 11:14:49 +0100

    Movin python config into languages

---
+++ languages/python.sh
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------
+# PYTHON
+# ----------------------------------------------------------------------
+# Python specific config
+
+# settins for virtual env wrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$REPODIR
+
