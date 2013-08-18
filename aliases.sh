262b1ffc13ccac8286a7f35556cc6f693a5a8ee8
Guy John <guy@rumblesan.com>
2013-08-18 12:53:52 +0100

    Combining aliase files

--- aliases.sh
+++ aliases.sh
@@ -1,22 +1,30 @@
-#!/bin/bash
+# ----------------------------------------------------------------------
+# ALIASES
+# ----------------------------------------------------------------------
 
-# Easier navigation: .., ..., ~ and -
+# Easier navigation
 alias ..="cd .."
 alias ...="cd ../.."
-alias ~="cd ~" # `cd` is probably faster to type though
+alias ....="cd ../../.."
+alias ~="cd ~"
 alias -- -="cd -"
 
 # Local IP addresses
 alias lip="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 
+# WAN IP Address
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+if $(gls &>/dev/null); then
+# If coreutils is installed use GNU ls with --color switch
+    alias ls="gls -h --color"
+    alias la="gls -ha --color"
+    alias ll="gls -hl --color"
+    alias lal="gls -hal --color"
+fi
+
 # Turn on colors for grep
 alias grep="grep --color"
-
-# By default, use normal ls and --color switch
-alias ls="ls -h --color"
-alias la="ls -ha --color"
-alias ll="ls -hl --color"
-alias lal="ls -hal --color"
 
 # Makes it easy to grep the ps output
 alias psgrep="ps aux | grep -v grep | grep"
