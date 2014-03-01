70fc2b9a2959db7a52f47cd2be4ea61775703fa3
Guy John <guy@rumblesan.com>
2014-03-01 14:11:25 +0000

    Adding ocaml language settings

---
+++ languages/ocaml.sh
@@ -0,0 +1,8 @@
+# ----------------------------------------------------------------------
+# OCAML
+# ----------------------------------------------------------------------
+# Ocaml specific config
+
+# OPAM configuration
+. /Users/guy/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
+
