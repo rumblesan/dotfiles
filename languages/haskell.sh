9a4540a207c42851eda1a39c41d0df915aafb6ea
Guy John <guy@rumblesan.com>
2014-05-11 12:42:00 +0100

    Tweaking haskell settings

--- languages/haskell.sh
+++ languages/haskell.sh
@@ -4,15 +4,5 @@
 # Haskell specific config
 
 # Add cabal bin dir to path
-PATH=$PATH:~/Library/Haskell/bin
-
-# Simpler way to use hsenv
-haskell_env()
-{
-    if [ -f ./.hsenv/bin/activate ]; then
-        source ./.hsenv/bin/activate
-    else
-        echo "No HSEnv present"
-    fi
-}
+PATH=~/Library/Haskell/bin:$PATH
 
