9ae45d24d1b481488f6682d2f9940b3b8b44b7c5
Guy John <guy@rumblesan.com>
2014-06-22 20:58:50 +0100

    get email script now sends notifications on new mail

--- scripts/getemail.sh
+++ scripts/getemail.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 
 BIN_DIR="/opt/boxen/homebrew/bin"
-DATE=`date +%F - %T`
+DATE=`date +%F-%T`
 
-echo "\n"
+echo ""
 echo "$DATE: Retrieving Email"
 $BIN_DIR/offlineimap -u basic
 
-echo "\n"
+echo ""
 echo "$DATE: Indexing Email"
 $BIN_DIR/notmuch new
+
+NEWMAIL=$("$BIN_DIR/notmuch" count tag:unread)
+if [ "$NEWMAIL" != "0" ]; then
+    echo "New mail received"
+    "$BIN_DIR/terminal-notifier" -title "New mail" -message "You have new Emails"
+fi
+
