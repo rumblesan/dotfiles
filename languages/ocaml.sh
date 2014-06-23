419e14e688d115d287dcd62bebd238e30a59d35a
Guy John <guy@rumblesan.com>
2014-06-23 08:39:26 +0100

    Removing ocaml config

--- languages/ocaml.sh
+++
@@ -1,8 +0,0 @@
-# ----------------------------------------------------------------------
-# OCAML
-# ----------------------------------------------------------------------
-# Ocaml specific config
-
-# OPAM configuration
-. /Users/guy/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
-
