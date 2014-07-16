4e332db450177d3ccc3c59c2a9f24d9ff45560b2
Guy John <guy@rumblesan.com>
2014-07-16 20:21:49 +0100

    Removing new email notification

--- scripts/getemail.sh
+++ scripts/getemail.sh
@@ -11,9 +11,3 @@ echo ""
 echo "$DATE: Indexing Email"
 $BIN_DIR/notmuch new
 
-NEWMAIL=$("$BIN_DIR/notmuch" count tag:unread)
-if [ "$NEWMAIL" != "0" ]; then
-    echo "New mail received"
-    "$BIN_DIR/terminal-notifier" -title "New mail" -message "You have new Emails"
-fi
-
