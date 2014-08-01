52f7ab67fffa2d56281a014c48cb12c8cf3bd8a6
Guy John <guy@rumblesan.com>
2014-08-01 11:29:10 +0100

    Updating keyremap stuff to be karabiner

--- misc/karabiner/setup.sh
+++ misc/karabiner/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PRIVATEDIR="$HOME/Library/Application Support/KeyRemap4MacBook"
+PRIVATEDIR="$HOME/Library/Application Support/Karabiner"
 
 if [ -d "$PRIVATEDIR" ]; then
 
@@ -19,9 +19,9 @@ if [ -d "$PRIVATEDIR" ]; then
 
     fi
 
-    ln -s "$HOME/.dotfiles/misc/keyremap/private.xml" "$PRIVATEDIR/private.xml"
+    ln -s "$HOME/.dotfiles/misc/karabiner/private.xml" "$PRIVATEDIR/private.xml"
 else
-    echo "$PRIVATEDIR doesn't exist! Is KeyRemap4MacBook installed?"
+    echo "$PRIVATEDIR doesn't exist! Is Karabiner installed?"
     exit 1
 fi
 
