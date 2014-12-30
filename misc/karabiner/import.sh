90380a5f13bdd765daab165a7c10ce5c805abd11
Guy John <guy@rumblesan.com>
2014-12-30 17:16:03 +0000

    Updating karabiner import script

--- misc/karabiner/import.sh
+++ misc/karabiner/import.sh
@@ -6,17 +6,19 @@ $cli set remap.f11_to_volumedown_all 1
 /bin/echo -n .
 $cli set remap.f10_to_musicnext_all 1
 /bin/echo -n .
-$cli set remap.drop_escape 1
-/bin/echo -n .
 $cli set remap.f9_to_musicplay_all 1
 /bin/echo -n .
 $cli set private.command_to_escape 1
+/bin/echo -n .
+$cli set private.functions_aS_MEDIA.CTRL_music_controls 1
 /bin/echo -n .
 $cli set remap.f8_to_musicprev_all 1
 /bin/echo -n .
 $cli set repeat.wait 15
 /bin/echo -n .
 $cli set remap.f12_to_volumeup_all 1
+/bin/echo -n .
+$cli set private.functions_as_media.ctrl_volume_controls 1
 /bin/echo -n .
 $cli set parameter.keyoverlaidmodifier_timeout 300
 /bin/echo -n .
