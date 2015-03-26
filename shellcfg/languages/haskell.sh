fb50794123bab0a8fc926c2479329a6e437b11f4
Guy John <guy@rumblesan.com>
2015-03-26 10:20:53 +0000

    Adding haskell ghc brew cask folder to path

--- shellcfg/languages/haskell.sh
+++ shellcfg/languages/haskell.sh
@@ -6,7 +6,7 @@
 # Add cabal bin dir to path
 if [[ "$OSTYPE" == "darwin"* ]]
 then
-    PATH=~/Library/Haskell/bin:$PATH
+    PATH=~/Library/Haskell/bin:$PATH:/opt/homebrew-cask/Caskroom/ghc/7.8.4-r0/ghc-7.8.4.app/Contents/bin
 else
     PATH=~/.cabal/bin:$PATH
 fi
