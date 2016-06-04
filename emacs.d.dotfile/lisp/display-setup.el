91a3dcf28d04a8d10e7be778005c5ab6c74b0cfd
Guy John <guy@rumblesan.com>
2016-06-04 12:57:04 +0100

    Make sure emacs doesn't complain if no GUI version

--- emacs.d.dotfile/lisp/display-setup.el
+++ emacs.d.dotfile/lisp/display-setup.el
@@ -12,8 +12,11 @@
 
 ;; Don't display the menu bar, tool bar or scroll bar
 (menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+(if (display-graphic-p)
+  (progn
+    (tool-bar-mode -1)
+    (scroll-bar-mode -1)
+    ))
 
 (set-frame-font "Menlo for Powerline-14")
 
