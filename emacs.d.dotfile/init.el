638676074ab41ced264089bc18ff7ed1c600d9e2
Guy John <guy@rumblesan.com>
2016-06-04 12:11:01 +0100

    move display settings into display setup file

--- emacs.d.dotfile/init.el
+++ emacs.d.dotfile/init.el
@@ -1,22 +1,9 @@
-;; Don't display the menu bar, tool bar or scroll bar
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq inhibit-startup-screen t)
-
 (setq my-lisp-dir
     (expand-file-name "lisp" user-emacs-directory))
 
 (add-to-list 'load-path my-lisp-dir)
 
-(set-frame-font "Menlo for Powerline-14")
-
 (setq make-backup-files nil)
-
-(setq initial-scratch-message nil)
-
-(global-linum-mode)
 
 ;;; Prevent Extraneous Tabs
 (setq-default indent-tabs-mode nil)
--- emacs.d.dotfile/lisp/display-setup.el
+++ emacs.d.dotfile/lisp/display-setup.el
@@ -6,4 +6,18 @@
 
 (enable-theme 'solarized)
 
+;; Don't display the menu bar, tool bar or scroll bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(set-frame-font "Menlo for Powerline-14")
+
+(setq initial-scratch-message nil)
+(setq inhibit-startup-screen t)
+
+
+(global-linum-mode)
+
+
 (provide 'display-setup)
