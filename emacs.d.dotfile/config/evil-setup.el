e79135417dc74c18b89cf7987cbfe6eb2549eb14
Guy John <guy@rumblesan.com>
2016-06-06 22:36:25 +0100

    Remove unnecessary key bindings

--- emacs.d.dotfile/config/evil-setup.el
+++ emacs.d.dotfile/config/evil-setup.el
@@ -9,11 +9,6 @@
   "b" 'helm-buffers-list
   )
 
-(define-key evil-normal-state-map "\C-h" 'evil-window-left)
-(define-key evil-normal-state-map "\C-j" 'evil-window-down)
-(define-key evil-normal-state-map "\C-k" 'evil-window-up)
-(define-key evil-normal-state-map "\C-l" 'evil-window-right)
-
 (define-key evil-motion-state-map ";" 'evil-ex)
 
 (setq evil-shift-width 4)
