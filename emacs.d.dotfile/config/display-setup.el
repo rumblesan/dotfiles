e84e10be7d58dfac166e22830a559d95e011dcd5
Guy John <guy@rumblesan.com>
2016-06-07 14:39:14 +0100

    Using custom spaceline

--- emacs.d.dotfile/config/display-setup.el
+++ emacs.d.dotfile/config/display-setup.el
@@ -8,8 +8,40 @@
 
 (ensure-package-installed 'spaceline)
 (require 'spaceline)
+(require 'spaceline-segments)
 (require 'spaceline-config)
-(spaceline-spacemacs-theme)
+
+(setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+(spaceline-install 'rumblesan
+                   `((evil-state :face highlight-face)
+                     auto-compile
+                     major-mode
+                     (process :when active)
+                     ((flycheck-error flycheck-warning flycheck-info)
+                      :when active)
+                     (minor-modes :when active)
+                     (mu4e-alert-segment :when active)
+                     (erc-track :when active)
+                     (version-control :when active)
+                     (org-pomodoro :when active)
+                     (org-clock :when active))
+
+                   `(which-function
+                     (python-pyvenv :fallback python-pyenv)
+                     (battery :when active)
+                     selection-info
+                     input-method
+                     ((buffer-encoding-abbrev
+                       point-position
+                       line-column)
+                      :separator " | ")
+                     (global :when active)
+                     buffer-position
+                     hud))
+
+(setq-default mode-line-format '("%e" (:eval (spaceline-ml-rumblesan))))
+
+
 (spaceline-helm-mode)
 
 ;; Don't display the menu bar, tool bar or scroll bar
