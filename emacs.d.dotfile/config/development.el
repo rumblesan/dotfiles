ab4c3d3bf6faa231a12d1de200098bb6c421d431
Guy John <guy@rumblesan.com>
2016-06-08 10:34:04 +0100

    Make flycheck errors appear quicker

--- emacs.d.dotfile/config/development.el
+++ emacs.d.dotfile/config/development.el
@@ -2,9 +2,12 @@
 
 (ensure-package-installed 'flycheck)
 (require 'flycheck)
+
 (with-eval-after-load 'flycheck
     (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 (setq-default flycheck-emacs-lisp-load-path 'inherit)
+(setq flycheck-display-errors-delay 0.1)
+
 (global-flycheck-mode)
 
 (provide 'development)
