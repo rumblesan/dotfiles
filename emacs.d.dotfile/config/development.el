6a5381e22cef51f16c94224b2d7ef43be00ffeb0
Guy John <guy@rumblesan.com>
2016-06-09 15:46:39 +0100

    Show matching parens

--- emacs.d.dotfile/config/development.el
+++ emacs.d.dotfile/config/development.el
@@ -4,10 +4,14 @@
 (require 'flycheck)
 
 (with-eval-after-load 'flycheck
-    (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 (setq-default flycheck-emacs-lisp-load-path 'inherit)
 (setq flycheck-display-errors-delay 0.1)
 
 (global-flycheck-mode)
+
+(show-paren-mode 1)
+(if (boundp 'show-paren-delay)
+    (setq show-paren-delay 0))
 
 (provide 'development)
