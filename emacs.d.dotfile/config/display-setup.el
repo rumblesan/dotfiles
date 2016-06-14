ba8ff9b339e38f6e69b0a38c8c37501e641c630c
Guy John <guy@rumblesan.com>
2016-06-14 22:06:58 +0100

    Move to custom powerline theme

--- emacs.d.dotfile/config/display-setup.el
+++ emacs.d.dotfile/config/display-setup.el
@@ -6,49 +6,10 @@
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-(setq frame-background-mode 'dark)
-
 (require 'moe-theme)
 (moe-dark)
 
-(ensure-package-installed 'spaceline)
-(require 'spaceline)
-(require 'spaceline-segments)
-(require 'spaceline-config)
-
-(setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-(spaceline-install 'rumblesan
-                   `((evil-state :face highlight-face)
-                     auto-compile
-                     '(buffer-modified buffer-size buffer-id remote-host)
-                     major-mode
-                     (process :when active)
-                     ((flycheck-error flycheck-warning flycheck-info)
-                      :when active)
-                     (minor-modes :when active)
-                     (mu4e-alert-segment :when active)
-                     (erc-track :when active)
-                     (version-control :when active)
-                     (org-pomodoro :when active)
-                     (org-clock :when active))
-
-                   `(which-function
-                     (python-pyvenv :fallback python-pyenv)
-                     (battery :when active)
-                     selection-info
-                     input-method
-                     ((buffer-encoding-abbrev
-                       point-position
-                       line-column)
-                      :separator " | ")
-                     (global :when active)
-                     buffer-position
-                     hud))
-
-(setq-default mode-line-format '("%e" (:eval (spaceline-ml-rumblesan))))
-
-
-(spaceline-helm-mode)
+(require 'rumblesan-powerline)
 
 ;; Don't display the menu bar, tool bar or scroll bar
 (menu-bar-mode -1)
---
+++ emacs.d.dotfile/config/rumblesan-powerline.el
@@ -0,0 +1,79 @@
+;;; rumblesan-powerline-el -- Rumblesan Modeline theme
+
+(require 'packages)
+(ensure-package-installed 'powerline)
+
+(require 'powerline)
+(require 'evil)
+
+;;; before requiring powerline-evil
+(defface powerline-evil-base-face
+  '((t (:foreground "black" :inherit mode-line)))
+  "Base face for powerline evil faces."
+  :group 'powerline)
+
+(require 'powerline-evil)
+(require 'moe-theme)
+
+(moe-theme-set-color 'cyan)
+(powerline-moe-theme)
+
+
+(defun powerline-rumblesan-theme ()
+  "Custom modeline"
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id nil))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (evil-face (powerline-evil-face))
+                          (evil-mode-line-tag (upcase (concat (symbol-name evil-state))))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+							  (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw evil-mode-line-tag evil-face 'l)
+                                     (powerline-raw " " evil-face)
+                                     (funcall separator-left evil-face mode-line)
+                                     (powerline-raw "%*" mode-line 'l)
+                                     (powerline-buffer-size mode-line 'l)
+                                     (powerline-buffer-id mode-line-buffer-id 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (powerline-raw " ")
+                                     (funcall separator-left mode-line face1)
+                                     (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-raw "|" face1)
+                                     (powerline-process face1)
+                                     (powerline-minor-modes face1)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+				     (unless window-system
+				       (powerline-raw (char-to-string #xe0a1) face1 'l))
+				     (powerline-raw "%4l" face1 'l)
+				     (powerline-raw ":" face1 'l)
+				     (powerline-raw "%3c" face1 'r)
+				     (funcall separator-right face1 mode-line)
+				     (powerline-raw " ")
+                                     (powerline-raw "%6p" mode-line 'r)
+                                     (powerline-hud face2 face1))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+			     (powerline-render rhs)))))))
+
+(powerline-rumblesan-theme)
+
+(provide 'rumblesan-powerline)
+
