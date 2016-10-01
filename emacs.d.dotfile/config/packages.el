c6b7ef6507956f685c08773570702d48bcec3f1c
Guy John <guy@rumblesan.com>
2016-10-01 10:43:49 +0100

    Simplifying emacs package bootstrapping

--- emacs.d.dotfile/config/packages.el
+++ emacs.d.dotfile/config/packages.el
@@ -1,18 +1,7 @@
 ;;; packages.el --- Useful stuff for setting up packages
 
 (require 'package)
-
-;; Detect online status, from ESK
-(require 'cl)
-(defun esk-online? ()
-  (if (and (functionp 'network-interface-list)
-           (network-interface-list))
-      (some (lambda (iface) (unless (equal "lo" (car iface))
-                         (member 'up (first (last (network-interface-info
-                                                   (car iface)))))))
-            (network-interface-list))
-    t))
-
+(setq package-enable-at-startup nil)
 (dolist (source '(("org" . "http://orgmode.org/elpa")
                   ("gnu" . "https://elpa.gnu.org/packages/")
                   ("melpa" . "http://melpa.org/packages/")
@@ -22,11 +11,10 @@
 
 (setq use-package-always-ensure t)
 (package-initialize)
-(when (esk-online?)
-  (unless package-archive-contents
-    (package-refresh-contents)
-    (package-install 'use-package)
-    ))
 
-(require 'use-package)
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (provide 'packages)
