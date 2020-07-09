3976d7263d4a25f9662c19c0622deda4d25c6371
Guy John <guy@rumblesan.com>
2020-07-09 09:23:00 +0100

    better lazy loading of pyenv and nodenv

--- shellcfg/languages/javascript.sh
+++ shellcfg/languages/javascript.sh
@@ -4,5 +4,29 @@
 # Javascript specific config
 
 if [ -n "$(command -v nodenv)" ]; then
-  eval "$(nodenv init -)"
+  export PATH="${HOME}/.nodenv/shims:${PATH}"
+
+  function nodenv() {
+      unset -f nodenv
+
+      export NODENV_SHELL=zsh
+      source '/usr/local/Cellar/nodenv/1.3.2/libexec/../completions/nodenv.zsh'
+      command nodenv rehash 2>/dev/null
+      nodenv() {
+        local command
+        command="${1:-}"
+        if [ "$#" -gt 0 ]; then
+          shift
+        fi
+
+        case "$command" in
+        rehash|shell)
+          eval "$(nodenv "sh-$command" "$@")";;
+        *)
+          command nodenv "$command" "$@";;
+        esac
+      }
+
+      nodenv $@
+  }
 fi
--- shellcfg/languages/python.sh
+++ shellcfg/languages/python.sh
@@ -1,3 +1,29 @@
 if [ -n "$(command -v pyenv)" ]; then
-  eval "$(pyenv init -)"
+  export PATH="${HOME}/.pyenv/shims:${PATH}"
+
+  function pyenv() {
+      unset -f pyenv
+
+      export PYENV_SHELL=zsh
+      source '/usr/local/Cellar/pyenv/1.2.19/libexec/../completions/pyenv.zsh'
+      command pyenv rehash 2>/dev/null
+      pyenv() {
+        local command
+        command="${1:-}"
+        if [ "$#" -gt 0 ]; then
+          shift
+        fi
+
+        case "$command" in
+        rehash|shell)
+          eval "$(pyenv "sh-$command" "$@")";;
+        *)
+          command pyenv "$command" "$@";;
+        esac
+      }
+
+      pyenv $@
+  }
+
+
 fi
