b0e1d04f469f1330685d6732a232b615b8faacf1
Guy John <guy@rumblesan.com>
2024-11-22 22:18:33 +0000

    correctly setting env vars for macos

--- shellcfg/zshrc-osx.sh
+++ shellcfg/zshrc-osx.sh
@@ -17,10 +17,11 @@ if [ -n $(command -v brew) ]; then
   # if brew and coreutils are installed, then add the gnubin to the path
   if [ -n $(command -v gls) ]; then
 
-    PATH="$BREWPATH/opt/coreutils/libexec/gnubin:$PATH"
-    MANPATH="$BREWPATH/opt/coreutils/libexec/gnuman:$MANPATH"
-    CPATH="/usr/local"
-    C_INCLUDE_PATH="/usr/local/include"
+    export PATH="$BREWPATH/opt/coreutils/libexec/gnubin:$PATH"
+    export MANPATH="$BREWPATH/opt/coreutils/libexec/gnuman:$MANPATH"
+    export CPATH=/opt/homebrew/include
+    export LIBRARY_PATH=/opt/homebrew/lib
+    export C_INCLUDE_PATH="/usr/local/include"
 
   fi
 
