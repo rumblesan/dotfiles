9e072dcb0274181bf1879bc511482ddf9d5fcece
Guy John <guy@rumblesan.com>
2025-02-04 20:33:51 +0000

    Tweaking python shell setup

--- shellcfg/languages/python.sh
+++ shellcfg/languages/python.sh
@@ -3,38 +3,14 @@
 # ----------------------------------------------------------------------
 # Python specific config
 
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-
 if [ -n "$(command -v pyenv)" ]; then
-  export PATH="${HOME}/.pyenv/shims:${PATH}"
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+  export PYENV_ROOT="$HOME/.pyenv"
 
-  function pyenv() {
-      unset -f pyenv
+  pyenv() {
+      eval "$(command pyenv init - zsh)"
 
-      export PYENV_SHELL=zsh
-      local brew_path=`brew --prefix`
-      local version=`command pyenv -v | awk '{print $2}'`
-      source "${brew_path}/Cellar/pyenv/${version}/completions/pyenv.zsh"
-      command pyenv rehash 2>/dev/null
-      pyenv() {
-        local command
-        command="${1:-}"
-        if [ "$#" -gt 0 ]; then
-          shift
-        fi
-
-        case "$command" in
-        activate|deactivate|rehash|shell)
-          eval "$(pyenv "sh-$command" "$@")"
-          ;;
-        *)
-          command pyenv "$command" "$@"
-          ;;
-        esac
-      }
-      pyenv $@
-
-      eval "$(pyenv virtualenv-init -)"
+      pyenv "$@"
   }
 
 fi
